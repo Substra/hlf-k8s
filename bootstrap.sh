885871b8ca9bf350e1c0878a0972f7a5ac8c08e9
GuillaumeCisco <guillaumecisco@gmail.com>
2019-03-04 17:33:56 +0100

    Rename fabric images to substra images - Do not pull fabric-ca images as fabric-ca is now a submodules

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,27 +16,12 @@ export THIRDPARTY_IMAGE_VERSION=0.4.13
 export ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')")
 export MARCH=$(uname -m)
 
-dockerCaPull() {
-      local CA_TAG=$1
-      echo "==> FABRIC CA IMAGE"
-      echo
-      docker pull hyperledger/fabric-ca:$CA_TAG
-      docker tag hyperledger/fabric-ca:$CA_TAG hyperledger/fabric-ca
-}
-
 createCustomDockerImages() {
     for dir in $BASEDIR/images/*/; do
         dir=`basename $dir`
         docker build -t substra/$dir $BASEDIR/images/$dir
     done
 }
-
-# starting with 1.2.0, multi-arch images will be default
-: ${CA_TAG:="$CA_VERSION"}
-
-echo "===> Pulling fabric ca Image"
-dockerCaPull ${CA_TAG}
-
 
 echo "===> List out hyperledger docker images"
 docker images | grep hyperledger/fabric-ca*
