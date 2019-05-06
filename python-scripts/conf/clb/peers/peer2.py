0405beaa48d01ab82ccce9e7ffdaae8208de7d85
Kelvin Moutet <kelvin.moutet@owkin.com>
2019-05-06 11:23:57 +0200

    Add a third org (clb).

---
+++ python-scripts/conf/clb/peers/peer2.py
@@ -0,0 +1,31 @@
+peer2 = {
+    'name': 'peer2',
+    'pass': 'peer2pw',
+    'host': 'peer2-clb',
+    'port': {
+        'internal': 7051,
+        'external': 12051
+    },
+    'anchor': False,
+    'tls': {
+        'dir': {
+            'external': '/substra/data/orgs/clb/tls/peer2',
+            'internal': '/etc/hyperledger/fabric/tls'
+        },
+        'client': {
+            'dir': 'client',
+            'cert': 'client.crt',
+            'key': 'client.key',
+            'ca': 'client.pem'
+        },
+        'server': {
+            'dir': 'server',
+            'cert': 'server.crt',
+            'key': 'server.key',
+            #  paradoxically, this will not be a tls certificate,
+            #  but will be put by fabric-ca inside tlscacerts directory
+            # it will be equal to org['ca']['certfile']
+            'ca': 'server.pem'
+        },
+    }
+}
---
+++ python-scripts/conf/clb/users/user.py
@@ -0,0 +1,5 @@
+user = {
+    'name': 'user-clb',
+    'pass': 'user-clbpw',
+    'home': '/substra/data/orgs/clb/user'
+}
