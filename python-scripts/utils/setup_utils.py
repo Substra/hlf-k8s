5b6ba61c78082ff1361d6d334509b798ac1e9fa1
GuillaumeCisco <guillaumecisco@gmail.com>
2019-05-27 17:32:34 +0200

    save progress

--- python-scripts/utils/setup_utils.py
+++ python-scripts/utils/setup_utils.py
@@ -2,6 +2,8 @@ import os
 from subprocess import call
 from shutil import copytree
 
+from hfc.fabric_ca.caservice import ca_service
+
 from .common_utils import waitPort, completeMSPSetup, dowait
 
 
@@ -28,18 +30,27 @@ def configLocalMSP(org, user_name):
             'org_user_home': org_user_home
         }, flush=True)
 
-        enrollment_url = 'https://%(name)s:%(pass)s@%(host)s:%(port)s' % {
-            'name': user['name'],
-            'pass': user['pass'],
-            'host': org['ca']['host'],
-            'port': org['ca']['port']['internal']
-        }
+        # enrollment_url = 'https://%(name)s:%(pass)s@%(host)s:%(port)s' % {
+        #     'name': user['name'],
+        #     'pass': user['pass'],
+        #     'host': org['ca']['host'],
+        #     'port': org['ca']['port']['internal']
+        # }
 
-        call(['fabric-ca-client',
-              'enroll', '-d',
-              '-c', '/etc/hyperledger/fabric/fabric-ca-client-config.yaml',
-              '-u', enrollment_url,
-              '-M', org_user_msp_dir])  # :warning: override msp dir
+        target = "https://%s:%s" % (org['ca']['host'], org['ca']['port']['internal'])
+        cacli = ca_service(target=target,
+                           ca_certs_path=org['ca']['certfile'],
+                           ca_name=org['ca']['name'])
+        user = cacli.enroll( user['name'], user['pass'])
+        print(user._cert)
+        print(user._private_key)
+
+
+        # call(['fabric-ca-client',
+        #       'enroll', '-d',
+        #       '-c', '/etc/hyperledger/fabric/fabric-ca-client-config.yaml',
+        #       '-u', enrollment_url,
+        #       '-M', org_user_msp_dir])  # :warning: override msp dir
 
         # admincerts is required for configtxgen binary
         # will copy cert.pem from <user>/msp/signcerts to <user>/msp/admincerts
@@ -55,80 +66,58 @@ def enrollCABootstrapAdmin(org):
              org['ca']['port']['internal'])
     print('Enrolling with %(CA_NAME)s as bootstrap identity ...' % {'CA_NAME': org['ca']['name']}, flush=True)
 
-    enrollment_url = 'https://%(name)s:%(pass)s@%(host)s:%(port)s' % {
-        'name': org['users']['bootstrap_admin']['name'],
-        'pass': org['users']['bootstrap_admin']['pass'],
-        'host': org['ca']['host'],
-        'port': org['ca']['port']['internal']
-    }
-
-    call(['fabric-ca-client',
-          'enroll', '-d',
-          '-c', '/etc/hyperledger/fabric/fabric-ca-client-config.yaml',
-          '-u', enrollment_url])
-
-    # python sdk
-    # caClient = ca_service(target=org['ca']['url'],
-    #                       ca_certs_path=org['ca']['certfile']['internal'],
-    #                       ca_name=org['ca']['name'])
-    # enrollment = caClient.enroll(org['bootstrap_admin']['name'], org['bootstrap_admin']['pass'])
+    target = "https://%s:%s" % (org['ca']['host'], org['ca']['port']['internal'])
+    cacli = ca_service(target=target,
+                       ca_certs_path=org['ca']['certfile']['internal'],
+                       ca_name=org['ca']['name'])
+    bootstrap_admin = cacli.enroll(org['users']['bootstrap_admin']['name'], org['users']['bootstrap_admin']['pass'])
+    print(bootstrap_admin)
+    # TODO create cert file?
+    return bootstrap_admin
 
 
 def registerOrdererIdentities(org):
-    enrollCABootstrapAdmin(org)
+    badmin = enrollCABootstrapAdmin(org)
 
     for orderer in org['orderers']:
         print('Registering %(orderer_name)s with %(ca_name)s' % {'orderer_name': orderer['name'],
                                                                  'ca_name': org['ca']['name']},
               flush=True)
 
-        call(['fabric-ca-client',
-              'register', '-d',
-              '-c', '/etc/hyperledger/fabric/fabric-ca-client-config.yaml',
-              '--id.name', orderer['name'],
-              '--id.secret', orderer['pass'],
-              '--id.type', 'orderer'])
+        orderer = badmin.register(orderer['name'], orderer['pass'], 'orderer')
+        print(orderer)
 
         print('Registering admin identity with %(ca_name)s' % {'ca_name': org['ca']['name']}, flush=True)
 
-        # The admin identity has the "admin" attribute which is added to ECert by default
-        call(['fabric-ca-client',
-              'register', '-d',
-              '-c', '/etc/hyperledger/fabric/fabric-ca-client-config.yaml',
-              '--id.name', org['users']['admin']['name'],
-              '--id.secret', org['users']['admin']['pass'],
-              '--id.attrs', 'admin=true:ecert'])
+        user = badmin.register(org['users']['admin']['name'], org['users']['admin']['pass'],
+                               attrs=[{'admin': 'true:ecert'}])
+        print(user)
 
 
 def registerPeerIdentities(org):
-    enrollCABootstrapAdmin(org)
+    badmin = enrollCABootstrapAdmin(org)
     for peer in org['peers']:
         print('Registering %(peer_name)s with %(ca_name)s\n' % {'peer_name': peer['name'],
                                                                 'ca_name': org['ca']['name']}, flush=True)
-        call(['fabric-ca-client', 'register', '-d',
-              '-c', '/etc/hyperledger/fabric/fabric-ca-client-config.yaml',
-              '--id.name', peer['name'],
-              '--id.secret', peer['pass'],
-              '--id.type', 'peer'])
+
+        badmin.register(peer['name'], peer['pass'], 'peer')
 
     print('Registering admin identity with %(ca_name)s' % {'ca_name': org['ca']['name']}, flush=True)
 
     # The admin identity has the "admin" attribute which is added to ECert by default
-    call(['fabric-ca-client',
-          'register', '-d',
-          '-c', '/etc/hyperledger/fabric/fabric-ca-client-config.yaml',
-          '--id.name', org['users']['admin']['name'],
-          '--id.secret', org['users']['admin']['pass'],
-          '--id.attrs',
-          'hf.Registrar.Roles=client,hf.Registrar.Attributes=*,hf.Revoker=true,hf.GenCRL=true,admin=true:ecert,abac.init=true:ecert'
-          ])
+    attrs = [{'hf.Registrar.Roles': 'client'},
+             {'hf.Registrar.Attributess': '*'},
+             {'hf.Revoker': 'true'},
+             {'hf.GenCRL': 'true'},
+             {'admin': 'true:ecert'},
+             {'abac.init': 'true:ecert'}
+             ]
+    admin = badmin.register(org['users']['admin']['name'], org['users']['admin']['pass'], attrs=attrs)
+    print(admin)
 
     print('Registering user identity with %(ca_name)s\n' % {'ca_name': org['ca']['name']}, flush=True)
-    call(['fabric-ca-client',
-          'register', '-d',
-          '-c', '/etc/hyperledger/fabric/fabric-ca-client-config.yaml',
-          '--id.name', org['users']['user']['name'],
-          '--id.secret', org['users']['user']['pass']])
+    user = admin.register(org['users']['user']['name'], org['users']['user']['pass'])
+    print(user)
 
 
 def registerIdentities(conf):
