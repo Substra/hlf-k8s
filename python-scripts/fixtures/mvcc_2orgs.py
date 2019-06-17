0a58588929000f48913c0460782c2a0fd6c6dc1c
GuillaumeCisco <guillaumecisco@gmail.com>
2019-06-17 09:53:47 +0200

    Fixes + mvcc reproducible examples

--- python-scripts/fixtures/mvcc_2orgs.py
+++ python-scripts/fixtures/mvcc_2orgs.py
@@ -2,6 +2,7 @@ import asyncio
 import glob
 import json
 import os
+from multiprocessing import Process, Pool
 
 from subprocess import call
 
@@ -55,35 +56,40 @@ def invokeChainCode(fcn, args, org_name, peers):
     except:
         res = response
     finally:
+        print(res)
         return res
+
+
+def createTraintuples(fcn, args, org_name, peers):
+    invokeChainCode(fcn, args, org_name, peers)
 
 
 def setup():
     '''
-                    | |  (_)
-        _____      _| | ___ _ __
-       / _ \ \ /\ / / |/ / | '_ \
-      | (_) \ V  V /|   <| | | | |
-       \___/ \_/\_/ |_|\_\_|_| |_|
+                        | |  (_)
+            _____      _| | ___ _ __
+           / _ \ \ /\ / / |/ / | '_ \
+          | (_) \ V  V /|   <| | | | |
+           \___/ \_/\_/ |_|\_\_|_| |_|
     '''
 
     res = queryChaincode('queryObjectives', None, 'owkin', [cli.get_peer('peer1-owkin')])
     print(res)
 
     '''
-              | |                             | |           
-           ___| |__  _   _   _ __   __ _ _ __ | |_ ___  ___ 
-          / __| '_ \| | | | | '_ \ / _` | '_ \| __/ _ \/ __|
-         | (__| | | | |_| | | | | | (_| | | | | ||  __/\__ \
-          \___|_| |_|\__,_| |_| |_|\__,_|_| |_|\__\___||___/
-
+          | |                             | |           
+       ___| |__  _   _   _ __   __ _ _ __ | |_ ___  ___ 
+      / __| '_ \| | | | | '_ \ / _` | '_ \| __/ _ \/ __|
+     | (__| | | | |_| | | | | | (_| | | | | ||  __/\__ \
+      \___|_| |_|\__,_| |_| |_|\__,_|_| |_|\__\___||___/
+                                                   
     '''
 
     fcn = 'registerDataManager'
     args = {
         'name': 'ISIC 2018',
         'openerHash': 'ccbaa3372bc74bce39ce3b138f558b3a7558958ef2f244576e18ed75b0cea994',
-        'openerStorageAddress':'http://chunantes.substrabac:8001/dataset/ccbaa3372bc74bce39ce3b138f558b3a7558958ef2f244576e18ed75b0cea994/opener/',
+        'openerStorageAddress': 'http://chunantes.substrabac:8001/dataset/ccbaa3372bc74bce39ce3b138f558b3a7558958ef2f244576e18ed75b0cea994/opener/',
         'type': 'Images',
         'descriptionHash': '7a90514f88c70002608a9868681dd1589ea598e78d00a8cd7783c3ea0f9ceb09',
         'descriptionStorageAddress': 'http://chunantes.substrabac:8001/dataset/ccbaa3372bc74bce39ce3b138f558b3a7558958ef2f244576e18ed75b0cea994/description/',
@@ -98,9 +104,9 @@ def setup():
     else:
         datamanager_chunantes_key = 'ccbaa3372bc74bce39ce3b138f558b3a7558958ef2f244576e18ed75b0cea994'
 
-    res = queryChaincode('queryDataset', [json.dumps({'key': datamanager_chunantes_key})], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
+    res = queryChaincode('queryDataset', [json.dumps({'key': datamanager_chunantes_key})], 'chu-nantes',
+                         [cli.get_peer('peer1-chu-nantes')])
     print(res)
-
 
     # register train data on dataset chu nantes (will take dataset creator as worker)
     fcn = 'registerDataSample'
@@ -116,8 +122,7 @@ def setup():
     # debug purpose only
     else:
         data_chunantes_train_keys_1 = ["62fb3263208d62c7235a046ee1d80e25512fe782254b730a9e566276b8c0ef3a",
-                                  "42303efa663015e729159833a12ffb510ff92a6e386b8152f90f6fb14ddc94c9"]
-
+                                       "42303efa663015e729159833a12ffb510ff92a6e386b8152f90f6fb14ddc94c9"]
 
     # register test data on datamanager_chunantes
     args = {
@@ -132,9 +137,8 @@ def setup():
     # debug purpose only
     else:
         data_chunantes_test_keys_1 = ["61b113ac7142bdd1cc8a824cd29940ce0e22e2381b25e0efe34f64cad5a5ff9b",
-                     "0e597cec32d7f5b147c78002b134062923782ccac0e9cbfdd06a0298e7949172"]
+                                      "0e597cec32d7f5b147c78002b134062923782ccac0e9cbfdd06a0298e7949172"]
 
-    # create datamanager, test data and challenge on owkin
     '''
                     | |  (_)      
         _____      _| | ___ _ __  
@@ -250,39 +254,6 @@ def setup():
     else:
         algo_chunantes_1_key = '9ca7ffbdbb55156b0fb44a227c3c305b7f7300113b6008c662460cf0f8f7cc3a'
 
-    # create second algo on challenge Simplified skin lesion classification
-    args = {
-        'name': 'Logistic regression for balanced problem',
-        'hash': '7742aea2001ceb40e9ce8a37fa27237d5b2d1f574e06d48677af945cfdf42ec0',
-        'storageAddress': 'http://chunantes.substrabac:8001/algo/7742aea2001ceb40e9ce8a37fa27237d5b2d1f574e06d48677af945cfdf42ec0/file/',
-        'descriptionHash': '3b1281cbdd6ebfec650d0a9f932a64e45a27262848065d7cecf11fd7191b4b1f',
-        'descriptionStorageAddress': 'http://chunantes.substrabac:8001/algo/7742aea2001ceb40e9ce8a37fa27237d5b2d1f574e06d48677af945cfdf42ec0/description/',
-        'permissions': 'all'
-    }
-    algo_chunantes_2 = invokeChainCode(fcn, [json.dumps(args)], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
-
-    # create third algo
-    args = {
-        'name': 'Neural Network',
-        'hash': '0acc5180e09b6a6ac250f4e3c172e2893f617aa1c22ef1f379019d20fe44142f',
-        'storageAddress': 'http://chunantes.substrabac:8001/algo/0acc5180e09b6a6ac250f4e3c172e2893f617aa1c22ef1f379019d20fe44142f/file/',
-        'descriptionHash': 'b9463411a01ea00869bdffce6e59a5c100a4e635c0a9386266cad3c77eb28e9e',
-        'descriptionStorageAddress': 'http://chunantes.substrabac:8001/algo/0acc5180e09b6a6ac250f4e3c172e2893f617aa1c22ef1f379019d20fe44142f/description/',
-        'permissions': 'all'
-    }
-    algo_chunantes_3 = invokeChainCode(fcn, [json.dumps(args)], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
-
-    # create fourth algo
-    args = {
-        'name': 'Random Forest',
-        'hash': 'f2d9fd38e25cd975c49f3ce7e6739846585e89635a86689b5db42ab2c0c57284',
-        'storageAddress': 'http://chunantes.substrabac:8001/algo/f2d9fd38e25cd975c49f3ce7e6739846585e89635a86689b5db42ab2c0c57284/file/',
-        'descriptionHash': '4acea40c4b51996c88ef279c5c9aa41ab77b97d38c5ca167e978a98b2e402675',
-        'descriptionStorageAddress': 'http://chunantes.substrabac:8001/algo/f2d9fd38e25cd975c49f3ce7e6739846585e89635a86689b5db42ab2c0c57284/description/',
-        'permissions': 'all'
-    }
-    algo_chunantes_4 = invokeChainCode(fcn, [json.dumps(args)], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
-
     # query data of the dataset in chu nantes
     res = queryChaincode('queryDataManagers', [], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
     print(res)
@@ -291,90 +262,25 @@ def setup():
     print(res)
 
     fcn = 'createTraintuple'
-    args = {
-        'algoKey': algo_chunantes_1_key,
-        'objectiveKey': objective_chunantes_key,
-        'inModels': '',
-        'dataManagerKey': datamanager_chunantes_key,
-        'dataSampleKeys': ','.join([x for x in data_chunantes_train_keys_1]),
-        'flTask': '',
-        'rank': '',
-        'tag': 'foo'
-    }
-    traintuple = invokeChainCode(fcn, [json.dumps(args)], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
-
-    #recreation of traintuple should fail
-    if isinstance(traintuple, dict) and 'key' in traintuple:
-        traintuple_key = traintuple['key']
-    # debug purposes
-    else:
-        traintuple_key = traintuple.split('tkey: ')[1][0:-1]
-
-    args = {
-        'key': traintuple_key
-    }
-    invokeChainCode('logStartTrain', [json.dumps(args)], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
-
-    res = queryChaincode('queryTraintuples', [], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
-    print(res)
-
-    args = {
-        'key': traintuple_key,
-        'log': 'ok',
-        'outModel': {
-            'hash': '10060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568',
-            'storageAddress': 'http://chunantes.substrabac:8001/model/10060f1d9e450d98bb5892190860eee8dd48594f00e0e1c9374a27c5acdba568/file/',
-        },
-        'perf': 0.91,
-    }
-    invokeChainCode('logSuccessTrain', [json.dumps(args)], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
-    # go back to owkin for creating testtuple
-    '''
-                    | |  (_)      
-        _____      _| | ___ _ __  
-       / _ \ \ /\ / / |/ / | '_ \ 
-      | (_) \ V  V /|   <| | | | |
-       \___/ \_/\_/ |_|\_\_|_| |_|
-    '''
-
-    fcn = 'createTesttuple'
-    args = {
-        'traintupleKey': traintuple_key,
-        'dataManagerKey': datamanager_chunantes_key,
-        'dataSampleKeys': ','.join(data_chunantes_test_keys_1),
-        'tag': 'foo'
-    }
-    testtuple = invokeChainCode(fcn, [json.dumps(args)], 'owkin', [cli.get_peer('peer1-owkin')])
-
-    '''
-              | |                             | |           
-           ___| |__  _   _   _ __   __ _ _ __ | |_ ___  ___ 
-          / __| '_ \| | | | | '_ \ / _` | '_ \| __/ _ \/ __|
-         | (__| | | | |_| | | | | | (_| | | | | ||  __/\__ \
-          \___|_| |_|\__,_| |_| |_|\__,_|_| |_|\__\___||___/
-
-    '''
-
-    if isinstance(testtuple, dict) and 'key' in testtuple:
-        testtuple_key = testtuple['key']
-    # debug purposes
-    else:
-        testtuple_key = traintuple.split('tkey: ')[1][0:-1]
-
-    args = {
-        'key': testtuple_key,
-    }
-    invokeChainCode('logStartTest', [json.dumps(args)], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
-
-    args = {
-        'key': testtuple_key,
-        'log': 'ok',
-        'perf': 0.99,
-    }
-    invokeChainCode('logSuccessTest', [json.dumps(args)], 'chu-nantes', [cli.get_peer('peer1-chu-nantes')])
+    with Pool(processes=20) as pool:
+        pool.starmap(createTraintuples, [
+            (fcn,
+             [json.dumps({
+                 'algoKey': algo_chunantes_1_key,
+                 'objectiveKey': objective_chunantes_key,
+                 'inModels': '',
+                 'dataManagerKey': datamanager_chunantes_key,
+                 'dataSampleKeys': ','.join([x for x in data_chunantes_train_keys_1]),
+                 'flTask': '',
+                 'rank': '',
+                 'tag': str(i)
+             })],
+             'chu-nantes',
+             ['peer1-owkin', 'peer1-chu-nantes']
+             ) for i in range(0, 100)])
 
 
-def run(cli):
+def run():
     res = True
 
     # Invoke chaincode with 1st peers of each org
@@ -393,7 +299,7 @@ def run(cli):
         call(['touch', '/substra/data/log/fixtures.successful'])
     else:
         print('Loading fixtures failed.', flush=True)
-        call(['touch','/substra/data/log/fixtures.fail'])
+        call(['touch', '/substra/data/log/fixtures.fail'])
 
 
 if __name__ == "__main__":
@@ -403,4 +309,4 @@ if __name__ == "__main__":
 
     cli = init_cli(orgs)
 
-    run(cli)
+    run()
